f09c08b753a8768713ce94c4dd44c1a1f8a42fd5
iwona <iwona@IWKA>
2025-04-02 21:11:06 +0100

    adding js to science quiz

--- js/science-quiz.js
+++ js/science-quiz.js
@@ -16,3 +16,26 @@ async function fetchQuizQuestions(category, difficulty, amount = 10) {
         return []; // Return empty array when faild
     }
 }
+
+// Class is responsible for managing the quiz state, including navigation and user responses
+class QuizManager {
+    constructor(questionsContainer, navigationContainer, progressContainer) {
+        this.questionsContainer = questionsContainer; // Box for displaying questions
+        this.navigationContainer = navigationContainer; // Box for navigation buttons
+        this.progressContainer = progressContainer; // Box for showing quiz progress
+        this.questions = []; // Array to store quiz questions
+        this.currentQuestionIndex = 0; // Keeps track of the current question index
+        this.userAnswers = []; // Stores user-selected answers
+    }
+    
+    // Initiates quiz questions function
+    // This function is called when quiz questions are fetched
+    loadQuestions(questions) {
+        this.questions = questions;
+        this.currentQuestionIndex = 0;
+        this.userAnswers = new Array(questions.length).fill(null); // Initiates user answers array
+        this.renderQuestion(); // Provides the first question
+        this.renderNavigation(); // Shows navigation buttons
+        this.updateProgress(); // Update progress bar
+    }
+}
