0475ded9d3faf899314da8ff08dd85187c7a39fc
Rebecca Harrison <rebeccaharrison@free.fr>
2025-04-08 12:57:52 +0100

    Finish Page Design

--- js/history.js
+++ js/history.js
@@ -1,4 +1,5 @@
 var score = 0;
+var bonus = 0;
 
 function start_quiz() {
     document.getElementById("question1-slide").style.display = "block";
@@ -18,6 +19,15 @@ function question_right(count) {
     document.getElementById("question" + num + "-slide").style.display = "none";
     document.getElementById("result").innerHTML = score;
     console.log(score);
+}
+
+function bonus_right(count) {
+    var num = count;
+    bonus = bonus + 1;
+    document.getElementById("question" + num + "-right").style.display = "block";
+    document.getElementById("question" + num + "-slide").style.display = "none";
+    document.getElementById("bonus_result").innerHTML = bonus;
+    console.log(bonus);
 }
 
 function nextQuestion(count) {
